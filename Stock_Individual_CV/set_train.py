eec6678d56dfb2d531797c89ea3f9527fc9e866c
Jacob <jacccobbb2@gmail.com>
2024-03-16 17:14:36 +0800

    cv resnet

--- Stock_Individual_CV/set_train.py
+++ Stock_Individual_CV/set_train.py
@@ -6,6 +6,6 @@ def set_train():
     batch_size = 160
     init = True
     fp16_training = True
-    num_epochs = 500
+    num_epochs = 50
     lr = 0.00001
     return stock_symbol, end_date, num_class, batch_size, init, fp16_training, num_epochs, lr
--- Stock_Individual_CV/utils_test.py
+++ Stock_Individual_CV/utils_test.py
@@ -6,7 +6,6 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
 def test(model, dataloader):
-    
     s_pred = []
     s_true = []
     # print(next(model.parameters()).device)
@@ -21,30 +20,3 @@ def test(model, dataloader):
     y_test_tensor = torch.concat(s_true)
     accuracy = (torch.sign(y_pred_tensor) == torch.sign(y_test_tensor)).sum() / len(y_test_tensor)
     return y_pred_tensor, accuracy
-
-def test_encoderdecoder(model, src, dataloader):    
-    """
-    Return:
-    1. Concatenated y_pred for all batch
-    2. Y_pred for last batch
-    3. acc
-    """
-    s_pred = []
-    s_true = []
-    # print(next(model.parameters()).device)
-    for x, y in dataloader:
-        batch_size = x.shape[0]
-        break
-    with torch.no_grad():
-        for x, y in tqdm(dataloader):
-            if x.size(0) != batch_size:
-                continue
-            x = x.permute(2, 0, 1).to(device)
-            y = y.to(device)
-            memory, y_pred = model(src, x, True)
-            s_pred.append(y_pred.cpu().detach())
-            s_true.append(y.cpu())
-    y_pred_tensor = torch.concat(s_pred)
-    y_test_tensor = torch.concat(s_true)
-    accuracy = (torch.sign(y_pred_tensor) == torch.sign(y_test_tensor)).sum() / len(y_test_tensor)
-    return y_pred_tensor, y_pred, accuracy
